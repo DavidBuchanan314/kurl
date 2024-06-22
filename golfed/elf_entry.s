78b612cd4b1d48f1051ee3148018738917516158
David Buchanan <d@vidbuchanan.co.uk>
2024-06-22 18:16:57 +0100

    store _start inside elf headers

--- golfed/elf_entry.s
+++ golfed/elf_entry.s
@@ -11,10 +11,11 @@
 // elf64_ehdr
 elf_base:
 .ascii "\x7f" "ELF" // e_ident
-.byte 0x02 // ei_class
-.byte 0x01 // ei_data
-.byte 0x01 // ei_version
-.byte 0, 0, 0, 0, 0, 0, 0, 0, 0 // ident padding
+
+_start:
+ldr        w0,[sp]
+add        x1, sp, #0x08
+b          nearly_call_main
 
 .hword 2    // e_type
 .hword 0xb7 // e_machine
@@ -22,9 +23,13 @@ elf_base:
 
 .dword _start // e_entry
 .dword phdr_start-elf_base     // e_phoff (offset from elf base)
-.dword 0      // e_shoff (don't care)
 
-.word 0      // e_flags
+// e_shoff, e_flags:
+call_main:
+bl         main
+mov        x8,#0x5d
+svc        0x0      // sys__exit
+
 .hword 64    // e_ehsize (doesn't actually matter)
 .hword 0x38  // e_phentsize
 
@@ -34,16 +39,15 @@ phdr_start:
 .word 7         // p_flags (RWX)
 .dword 0        // p_offset
 .dword elf_base // p_vaddr
-.dword elf_base // p_paddr (can be anything)
+// p_paddr:
+
+nearly_call_main:
+sub        sp, sp, #0x10
+b          call_main
+
 .dword 0x1000   // p_filesz (overkill since we don't know the real value)
 .dword 0x100000 // p_memsz  (likewise)
 .dword 0x10000  // p_align (we really are aligned to this)
 
-_start: 
-ldr        w0,[sp]
-add        x1, sp, #0x08
-sub        sp, sp, #0x10
 
-bl         main
-mov        x8,#0x5d
-svc        0x0      // sys__exit
+
